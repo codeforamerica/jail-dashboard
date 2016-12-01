85e8306b73a68be509c9a8b3d6b300022c0b9d09
Christa Hartsock <christa@codeforamerica.org>
2016-12-01 12:28:32 -0800

    Add axis markers for pop cap bar chart
    * Set the values for the markers with environment variables
    * Use `dotenv-rails` gem to pull env variables from `.env`
    * Style marker axis with CSS

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,12 +1,24 @@
 $(document).ready( function() {
-  var hardCap = d3.select(".chart").attr("data-hard-cap");
-  var activeBookings = d3.select(".chart").attr("data-active-bookings");
+  var chart = d3.select(".chart");
 
-  var margin = { top: 100, right: 20, bottom: 60, left: 40 };
+  var parseIntAttr = function(element, attrName) {
+    return parseInt(element.attr(attrName), 10)
+  }
+
+  var markers = {
+    'softCap': parseIntAttr(chart, "data-soft-cap"),
+    'redZoneStart': parseIntAttr(chart, "data-red-zone-start"),
+    'hardCap':parseIntAttr(chart, "data-hard-cap"),
+    'maxPopulation': parseIntAttr(chart, "data-max-population"),
+  }
+
+  var activeBookings = parseIntAttr(chart, "data-active-bookings");
+
+  var margin = { top: 20, right: 20, bottom: 60, left: 40 };
   var width = 800 - margin.left - margin.right;
   var height = 200 - margin.top - margin.bottom;
 
-  var svg = d3.select('.chart')
+  var svg = chart
     .append('svg')
       .attr('height', height + margin.top + margin.bottom)
       .attr('width', width + margin.left + margin.right)
@@ -14,11 +26,19 @@ $(document).ready( function() {
       .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
   var xScale = d3.scaleLinear()
-          .domain([0, hardCap])
+          .domain([0, markers.maxPopulation])
           .range([0, width])
 
   var bar = svg.append('rect')
+    .attr('class', 'active-bookings')
     .attr('width', xScale(activeBookings))
+    .attr('y', height / 2)
+    .attr('height', height / 2);
+
+  var redZone = svg.append('rect')
+    .attr('class', 'red-zone')
+    .attr('width', xScale(markers.hardCap - markers.redZoneStart))
+    .attr('x', xScale(markers.redZoneStart))
     .attr('height', height);
 
   svg.append('text')
@@ -27,9 +47,32 @@ $(document).ready( function() {
     .attr('y', height/2)
     .attr('text-anchor', 'middle')
 
+  var markerAxis = d3.axisTop(xScale)
+    .tickSize(height)
+    .tickValues(Object.values(markers))
+
   var xAxis = d3.axisBottom(xScale);
 
   svg.append('g')
     .attr('transform', 'translate(0, ' + height + ')')
+    .attr('class', 'marker-axis')
+    .call(markerAxis);
+
+  svg.append('g')
+    .attr('transform', 'translate(0, ' + height + ')')
       .call(xAxis);
-} );
+
+  var reverseLookup = function(objOriginal, comparedValue) {
+    var values = Object.values(objOriginal);
+
+    var index = values.indexOf(comparedValue);
+
+    return Object.keys(objOriginal)[index];
+  };
+
+  // Give the marker ticks their classes
+  d3.selectAll(".marker-axis .tick line")
+    .attr('class', function(node, index) {
+      return reverseLookup(markers, node);
+    });
+  } );
