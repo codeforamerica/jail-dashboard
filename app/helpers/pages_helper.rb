a3fcff6c6684a360e225b5b627c1a0de13841f15
Grayson Wright <wright.grayson@gmail.com>
2016-12-09 14:39:12 -0800

    Handle failed percentage calculations gracefully

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,11 @@
 module PagesHelper
   def present_percent(count, total)
-    percent_as_decimal = count.to_f / total.to_f * 100
+    percent_as_decimal = if total.zero?
+                           0
+                         else
+                           count.to_f / total.to_f * 100
+                         end
+
     number_to_percentage(percent_as_decimal, precision: 1)
   end
 end
