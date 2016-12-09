a981e8ca20c801d53fc824f7b0ae9f8ce3168ac4
Jen <j.aprahamian@gmail.com>
2016-12-09 12:18:13 -0800

    add healthcheck route without auth barrier

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
 
   root 'pages#home'
-
+  get 'healthcheck' => 'pages#healthcheck'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
