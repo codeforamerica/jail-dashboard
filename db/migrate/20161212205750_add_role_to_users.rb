b9dfcb7086bff60e427e169538072a37c11296ca
Jen <j.aprahamian@gmail.com>
2016-12-13 14:33:29 -0800

    fix syntax so hound stops barking

--- db/migrate/20161212205750_add_role_to_users.rb
+++ db/migrate/20161212205750_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :role, :string, default: "user"
+    add_column :users, :role, :string, default: 'user'
   end
 end
