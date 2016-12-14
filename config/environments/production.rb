8dc23ea6fb055447efa67fd997496348fa4f1195
Jen <j.aprahamian@gmail.com>
2016-12-14 14:38:39 -0800

    configure mail host for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
    enable_starttls_auto: true
   }
 
+  config.action_mailer.default_url_options = { :host => 'jaildashboard.herokuapp.com' }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
