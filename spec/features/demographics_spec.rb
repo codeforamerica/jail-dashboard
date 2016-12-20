11bc27316edc6de971ecc7c036153eeb1c652cce
Grayson Wright <wright.grayson@gmail.com>
2016-12-20 12:58:56 -0500

    Make breakout charts respond to filters
    * Move the breakout chart rendering logic into d3
    * Update the breakout charts whenever a filter is applied
    
    TODO:
    
    Breakout charts for a specific dimension
    don't respond to filters for that dimension.

--- spec/features/demographics_spec.rb
+++ spec/features/demographics_spec.rb
@@ -7,7 +7,7 @@ describe 'demographics' do
     login_as FactoryGirl.create(:user)
   end
 
-  it 'shows gender breakdown of actively booked people' do
+  it 'shows gender breakdown of actively booked people', :js do
     male = FactoryGirl.create(:person, gender: 'male')
     female = FactoryGirl.create(:person, gender: 'female')
     released = FactoryGirl.create(:person, gender: 'female')
@@ -18,13 +18,13 @@ describe 'demographics' do
 
     visit '/'
 
-    within('.demographics-gender') do
-      expect(page).to have_css('tr', text: 'Male 1 50.0%')
-      expect(page).to have_css('tr', text: 'Female 1 50.0%')
+    within('.filters') do
+      expect(page).to have_css('tr', text: 'male 1 50%')
+      expect(page).to have_css('tr', text: 'female 1 50%')
     end
   end
 
-  it 'shows race/ethnicity breakdown of actively booked people' do
+  it 'shows race/ethnicity breakdown of actively booked people', :js do
     Person::RACES.each do |race|
       active = FactoryGirl.create(:person, race: race)
       FactoryGirl.create(:booking, person: active)
@@ -37,12 +37,11 @@ describe 'demographics' do
 
     expected_percentage = number_to_percentage(
       100.0 / Person::RACES.count,
-      precision: 1,
+      precision: 0,
     )
-    within('.demographics-race') do
+    within('.filters') do
       Person::RACES.each do |race|
-        expect(page).
-          to have_css('tr', text: "#{race.titlecase} 1 #{expected_percentage}")
+        expect(page).to have_css('tr', text: "#{race} 1 #{expected_percentage}")
       end
     end
   end
